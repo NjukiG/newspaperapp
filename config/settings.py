eff447b9ab791e7923ac4ba996509bebfca5a2bf
George <gnjuki19@gmail.com>
2023-03-16 14:19:52 +0300

    Started a new articles app and added 3 articles from the admin page

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     # Local
     'accounts', # new
     'pages', # new
+    'articles', # new
 ]
 
 MIDDLEWARE = [
@@ -113,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi' # new
 
 USE_I18N = True
 
