947b97f51bbc7700d2f9881975b2c4d7ed9b9c8e
George <gnjuki19@gmail.com>
2023-03-16 14:27:45 +0300

    Added the urls, models and views for articles to be rendered in homepage and functional

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Article
+
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+
 
 # Create your views here.
