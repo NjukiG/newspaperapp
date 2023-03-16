2adef2221c351766b6fb639b4776fbde8f6b136a
George <gnjuki19@gmail.com>
2023-03-16 17:08:49 +0300

    Updated code to have authorizations on articles by different users. Only the authros are able to edit and delete original articles. Also, to create an article, user has to be logged in.

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin # new
+)
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView # new
 from django.views.generic.edit import UpdateView, DeleteView, CreateView # new
@@ -5,31 +9,43 @@ from django.urls import reverse_lazy # new
 from .models import Article
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):  # new
     model = Article
     template_name = 'article_list.html'
 
 
-class ArticleDetailView(DetailView): # new
+class ArticleDetailView(LoginRequiredMixin, DetailView): # new
     model = Article
     template_name = 'article_detail.html'
 
 
-class ArticleUpdateView(UpdateView): # new
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView): # new
     model = Article
     fields = ('title', 'body',)
     template_name = 'article_edit.html'
 
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleDeleteView(DeleteView): # new
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView): # new
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
 
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleCreateView(CreateView): # new
+
+class ArticleCreateView(LoginRequiredMixin, CreateView): # new
     model = Article
     template_name = 'article_new.html'
-    fields = ('title', 'body', 'author',)
+    fields = ('title', 'body') # new
+
+    def form_valid(self, form): # new
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 # Create your views here.
