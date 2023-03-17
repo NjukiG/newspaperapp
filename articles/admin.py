f6fe0fe98d02b8f52fc927eba3169454dc0f0002
George <gnjuki19@gmail.com>
2023-03-17 12:05:14 +0300

    App now able to create users, log in, log out, create articles, and comments and perform CRUD on them.

--- articles/admin.py
+++ articles/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import Article, Comment # new
 
+class CommentInline(admin.TabularInline): # new
+    model = Comment
 
-admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin): # new
+    inlines = [
+        CommentInline,
+    ]
+
+
+
+
+admin.site.register(Article, ArticleAdmin) # new
 admin.site.register(Comment) # new
 
 # Register your models here.
