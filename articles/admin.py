ee88f848608c5f772525c5daef29f78c9c61521d
George <gnjuki19@gmail.com>
2023-03-17 11:44:30 +0300

    Added a comment model and tested it out with a new comment on an article

--- articles/admin.py
+++ articles/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment # new
 
 
 admin.site.register(Article)
+admin.site.register(Comment) # new
 
 # Register your models here.
